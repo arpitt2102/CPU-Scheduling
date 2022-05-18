6251cbd7e1aacd896154d9776b7b867b948ce7e5
Arpit Thakkar <67607904+arpitt2102@users.noreply.github.com>
2022-05-19 01:21:52 +0530

    Add files via upload

---
+++ fcfs.c
@@ -0,0 +1,210 @@
+/*
+Family Name: Thakkar
+Given Name: Arpit Nileshbhai
+Student Number: 217632340
+EECS Login ID (the one you use to access the red server): arpit21
+YorkU email address (the one that appears in eClass): arpit21@my.yorku.ca
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include "sch-helpers.h"
+
+process processes[MAX_PROCESSES+1];
+process_queue rQueue;
+process_queue wQueue;
+
+//Creating temperory queue of struct process
+process *tempQ[MAX_PROCESSES+1];
+int tempQLength = 0;
+
+//Creating CPU with the numberOfProcesses parameter
+process *CPU[NUMBER_OF_PROCESSORS];
+
+//Global variables
+int nextP = 0;
+int totalTime = 0;
+int numberOfProcesses = 0;
+int totalCPUUti = 0;
+int totalRunProcess = 0;
+int totalWait = 0;
+int lastProcess = 0;
+int totalTurnaround = 0;
+int switches = 0;
+
+/* 
+We created this helper function named nextProcess. We will use it to check what is the 
+next process. If Ready queue is empty then we return null but if no then we assign nextElement of type process
+to the front of the queue. We also remove the element that was previously there.
+*/
+process *nextProcess(){
+    if(rQueue.size == 0){ 
+        return NULL;
+    }
+    process *nextElement = rQueue.front->data;
+    dequeueProcess(&rQueue);
+    return nextElement;
+}
+
+int main(){
+    int status = 0;
+    int i = 0;
+    /* 
+    We make the CPU processes NULL in order to get the new CPU everytime on the start 
+    of new process. That is why we added it to the top in the main menu.
+    */
+    for(i = 0; i < NUMBER_OF_PROCESSORS; i++){
+        CPU[i] = NULL;
+    }
+    int comingProcess = numberOfProcesses - nextP;
+    
+    //initializing queues
+    initializeProcessQueue(&rQueue);
+    initializeProcessQueue(&wQueue);
+
+    while((status = (readProcess(&processes[numberOfProcesses])))){
+        if (status == 1){
+            numberOfProcesses++;
+        }
+        //We check if number of processes are equal to 0 or larger than MAX_PROCESSES. 
+        //If so then we output the error message.
+        if(numberOfProcesses == 0 || 
+           numberOfProcesses > MAX_PROCESSES){
+            fprintf(stderr, "Either processes are zero or are more than maximum possible processes\n");
+            exit(-1);
+        }
+    }
+
+    qsort(processes,numberOfProcesses,sizeof(process *), compareByArrival);
+
+    while(1){
+        
+        /* 
+       If the run process becomes zero and there is nothing left in waiting queue then we can exit the while loop
+       and output the results.
+       */
+      if(totalRunProcess == 0  && wQueue.size == 0 && comingProcess == 0){
+          break;
+       }
+        //We will first add the processes in the temporary queue that we created. It will be sorted in the next qsort function.
+        while(nextP < numberOfProcesses && 
+              processes[nextP].arrivalTime <= totalTime){
+            tempQ[tempQLength++] = &processes[nextP++];
+        }
+        qsort(tempQ,tempQLength,sizeof(process *),compareByPID);
+        /*  
+        First we will move the prcesses that are finished to waiting queue.
+        And then the processes which are not finished, those will also move to waiting queue by freeing the CPU
+        */
+       int i = 0;
+       for(i = 0; i<NUMBER_OF_PROCESSORS; i++){
+           if(CPU[i] != NULL){
+               if(CPU[i]->bursts[CPU[i]->currentBurst].step == 
+                  CPU[i]->bursts[CPU[i]->currentBurst].length){
+                   (CPU[i])->currentBurst++;
+                   if (CPU[i]->currentBurst < CPU[i]->numberOfBursts){
+                       enqueueProcess(&wQueue, CPU[i]);
+                   }
+                   else {
+                       CPU[i]->endTime = totalTime;
+                   }
+                   CPU[i] = NULL;
+               }
+           }
+       }
+       /* 
+       We have sorted elements in temperory queue and we now enqueue that in ready queue. 
+       Then we reset the length of this temporary queue so that it can be used again.
+       */
+       for(i = 0; i<tempQLength; i++){
+           enqueueProcess(&rQueue, tempQ[i]);
+       }
+       tempQLength = 0; 
+       /* 
+       Now we find the next process and schedule it to the CPU.
+       */
+       for(i = 0; i<NUMBER_OF_PROCESSORS; i++){
+           if(CPU[i] == NULL) {
+               CPU[i] = nextProcess();
+           }
+       }
+       /* 
+       Now we will check if the first element in the waiting queue has completed I/O burst or not.
+       If yes, then we move that process to the temporary queue that we have created and dequeue the waiting queue.
+       Then we update the waiting queue with the burst steo]p of current function.
+       */
+      for(i = 0; i<wQueue.size; i++){
+          process *nextElement = wQueue.front->data;
+          dequeueProcess(&wQueue);
+          if(nextElement->bursts[nextElement->currentBurst].step 
+          == nextElement->bursts[nextElement->currentBurst].length){
+              nextElement->currentBurst++;
+              tempQ[tempQLength++] = nextElement;
+           }
+          else {
+            enqueueProcess(&wQueue, nextElement);
+          }
+       }
+      /* 
+       Now we should update the waiting and running processes as well as the CPU processes. 
+       We will use three for loops for updating the processes.
+       */
+      for(i = 0; i < wQueue.size; i++){
+          process *nextElement = wQueue.front->data;
+          dequeueProcess(&wQueue);
+          nextElement->bursts[nextElement->currentBurst].step++;
+          enqueueProcess(&wQueue,nextElement);
+      }
+      for(i = 0; i < rQueue.size; i++){
+          process *nextElement = rQueue.front->data;
+          dequeueProcess(&rQueue);
+          nextElement->waitingTime++;
+          enqueueProcess(&rQueue, nextElement);
+      }
+      for(i = 0; i < NUMBER_OF_PROCESSORS; i++){
+          if(CPU[i] != NULL){
+              CPU[i]->bursts[CPU[i]->currentBurst].step++;
+          }
+      }
+      /* 
+       Here we find the total running processes on CPU.
+       */
+      for(i = 0; i < NUMBER_OF_PROCESSORS; i++){
+          if(CPU[i] != NULL){
+              totalRunProcess++;
+          }
+      }
+      totalCPUUti = totalCPUUti + totalRunProcess;
+      totalTime++;
+    }
+    /* 
+    We calculate the total waitime and turnarouund time here.
+    WE wll use that in obtaining average waittime and turnaround time.
+    */
+    for(i = 0; i < numberOfProcesses; i++){
+        totalWait = totalWait + processes[i].waitingTime;
+        totalTurnaround += processes[i].endTime - processes[i].arrivalTime;
+        if(processes[i].endTime == totalTime) {
+            lastProcess = processes[i].pid;
+        }
+    }
+    /* 
+    We use the value of waiting time and turnaround time to calculate
+    the average of values. Also CPU utilization is in % so we multiplied it with 100 
+    to obtain that.
+    */
+    double wait = totalWait / (double) numberOfProcesses;
+    double turnaround = totalTurnaround / (double) numberOfProcesses;
+    double utilization = (totalCPUUti * 100.0) / totalTime;
+
+    printf("Average waiting time: %.2f\n"
+            "Average Turnaround time: %.2f\n"
+            "Time taken by CPU to finish all processes: %d\n"
+            "Average CPU Utilization: %.1f\n"
+            "Context Switches: %d\n"
+            "PID of last process: %d\n", wait, turnaround, totalTime, utilization, switches, lastProcess);
+
+    return 0;
+}
---
+++ rr.c
@@ -0,0 +1,328 @@
+/*
+Family Name: Thakkar
+Given Name: Arpit Nileshbhai
+Student Number: 217632340
+EECS Login ID (the one you use to access the red server): arpit21
+YorkU email address (the one that appears in eClass): arpit21@my.yorku.ca
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include "sch-helpers.h"
+
+/* Global variables */
+/* processes array to store data */
+process processes[MAX_PROCESSES+1];		
+/* total number of processes */	
+int numberOfProcesses;
+/* next process in the queue */					
+int nextP;		
+/* Total wait time */						
+int totalWait;			
+/* Total context switches */				
+int contextSwitches;
+/* Total CPU utilization time */					
+int cpuTime;
+/* Total time to finish all processes */
+int totalTime;
+/* Total turnaround time */
+int turnaround;
+/* Time Quantum */
+int timeQ;
+
+/* Ready and Waiting Queue of struct process_queue */
+
+process_queue readyQ;
+process_queue waitQ;
+
+/* CPU pointer */
+
+process *cpu[NUMBER_OF_PROCESSORS];
+
+/* Temporary Queue for storing and sorting data */
+
+process *temQ[MAX_PROCESSES+1];
+int temQlen;
+
+/* Variables reset values */
+
+void resetVariables(void){
+	numberOfProcesses = 0;
+	nextP = 0;
+	totalWait = 0;
+	contextSwitches = 0;
+	cpuTime = 0;
+	totalTime = 0;
+	turnaround = 0;
+	temQlen = 0;
+}
+
+/* Counts the total incoming processes */
+
+int totalIncomingProcesses(void){
+	return numberOfProcesses - nextP;
+}
+
+/* Comparing process id */
+
+int compareProcessId(const void *a, const void *b){
+	process *first = (process *) a;
+	process *second = (process *) b;
+	if (first->pid < second->pid) return -1;
+    if (first->pid > second->pid) return 1;
+	return 0;
+}
+
+/* Currently runnning processes in the CPU */
+
+int totalRunningProcesses(void){
+	int runningProcesses = 0;
+	int i;
+	for (i = 0; i < NUMBER_OF_PROCESSORS; i++){
+		if (cpu[i] != NULL) runningProcesses++;
+	}
+	return runningProcesses;
+}
+
+/* Gets the next process from ready queue and then dequeue it 
+from the ready queue and returns it */
+
+process *nextProcess(void){
+	if (readyQ.size == 0){
+		return NULL;
+	}
+	process *nextElement = readyQ.front->data;
+	dequeueProcess(&readyQ);
+	return nextElement;
+}
+
+/* Adding the new coming processes to the temporary queue that we created earlier */
+
+void comingProcesses(void){
+	while(nextP < numberOfProcesses && processes[nextP].arrivalTime <= totalTime){
+		
+		/* Getting the address of next process */
+
+		temQ[temQlen] = &processes[nextP];
+		temQ[temQlen]->quantumRemaining = timeQ;
+		
+		/* Incrementing length of temporary queue and of next process */
+		
+		temQlen++;
+		nextP++;
+	}
+}
+
+/* Move the data from waiting queue to ready queue */
+
+void waitingProcesses(void){
+ 	int i;
+	int waitQSize = waitQ.size;
+ 	for(i = 0; i < waitQSize; i++){
+ 		process *nextElement = waitQ.front->data;
+
+		/* Dequeing elements from waiting queue */
+
+ 		dequeueProcess(&waitQ);
+
+		/* If current I/O burst is complete then transfer it to 
+		temporary queue else enqueue it again to waiting queue */
+
+ 		if(nextElement->bursts[nextElement->currentBurst].step == nextElement->bursts[nextElement->currentBurst].length){
+ 			nextElement->currentBurst++;
+ 			nextElement->quantumRemaining = timeQ;
+ 			nextElement->endTime = totalTime;
+ 			temQ[temQlen++] = nextElement;
+ 		}
+ 		else{
+ 			enqueueProcess(&waitQ, nextElement);
+ 		}
+ 	}
+}
+
+/* Moving the ready process into idle CPUs */
+
+void readyProcesses(void){
+ 	int i;
+ 	qsort(temQ, temQlen, sizeof(process*), compareProcessId);
+ 	for (i = 0; i < temQlen; i++){
+ 		enqueueProcess(&readyQ, temQ[i]);
+ 	}
+	temQlen = 0;
+
+	/* For each cpu check if it is free or not. If free, 
+	then assign it to next process in the queue. */
+
+ 	for (i = 0; i < NUMBER_OF_PROCESSORS; i++){
+ 		if (cpu[i] == NULL) cpu[i] = nextProcess();
+	}
+}
+
+/* Move processes which just completed their last burst to termination and 
+if it is not the last burst then move it to waiting queue */
+
+void runningProcess(void){
+	int num = 0;
+	process *preemptive[NUMBER_OF_PROCESSORS];
+ 	int i,j;
+ 	for (i = 0; i < NUMBER_OF_PROCESSORS; i++){
+ 		if (cpu[i] != NULL){
+
+			/* if current burst is completed then move to next burst */
+
+ 			if (cpu[i]->bursts[cpu[i]->currentBurst].step == cpu[i]->bursts[cpu[i]->currentBurst].length){
+ 				cpu[i]->currentBurst++;
+
+				/* If the cpu I/O burst is less than total number of 
+				burst of that cpu then add it to waiting queue to be exectued afterwards 
+				else it will exit and calculate the total time */
+
+ 				if (cpu[i]->currentBurst < cpu[i]->numberOfBursts){
+ 					enqueueProcess(&waitQ, cpu[i]);
+ 				}
+				
+				/* else terminate it */
+
+ 				else{
+ 					cpu[i]->endTime = totalTime;
+ 				}
+ 				cpu[i] = NULL;
+ 			}
+ 			else if(cpu[i]->quantumRemaining == 0){
+ 				preemptive[num] = cpu[i];
+ 				preemptive[num]->quantumRemaining = timeQ;
+ 				num++;
+ 				contextSwitches++;
+ 				cpu[i] = NULL;
+ 			}
+ 		}	
+ 	}
+
+	/* Sort the preemptive array by its process id and 
+	then adding it to the ready queue .*/
+
+ 	qsort(preemptive, num, sizeof(process*), compareProcessId);
+ 	for (j = 0; j < num; j++){
+ 		enqueueProcess(&readyQ, preemptive[j]);
+ 	}
+}
+
+int main(int argc, char *argv[]){
+	int i;
+	int readStatus = 0;
+	int lastPID;
+
+	/* argv[1] would be the time quantum that 
+	we are going to input along with our execution file*/
+
+	timeQ = atoi(argv[1]);
+
+	/* Initializing cpu's to empty */
+
+	for (i = 0; i < NUMBER_OF_PROCESSORS; i++){
+		cpu[i] = NULL;
+	}
+
+	/* Initializing variables to 0 and initializing process queues */
+
+	resetVariables();
+	initializeProcessQueue(&readyQ);
+	initializeProcessQueue(&waitQ);
+
+	while( (readStatus = (readProcess(&processes[numberOfProcesses]))) ){
+		if (readStatus == 1){
+			numberOfProcesses++;
+		}
+	}
+	
+	/* Sorting array of processes by its arrival time */
+
+	qsort(processes, numberOfProcesses, sizeof(process*), compareByArrival);
+
+	while (1){
+
+		/* Calling all the functions here */
+
+		comingProcesses();
+		runningProcess();
+		readyProcesses();
+		waitingProcesses();
+		
+		/* Updating the waiting queue */
+
+		for (i = 0; i < waitQ.size; i++){
+ 			process *nextElement = waitQ.front->data;
+ 			dequeueProcess(&waitQ);
+ 			nextElement->bursts[nextElement->currentBurst].step++;
+ 			enqueueProcess(&waitQ, nextElement);
+ 		}
+
+		/* Updating the ready queue */
+
+ 		for (i = 0; i < readyQ.size; i++){
+ 			process *nextElement = readyQ.front->data;
+ 			dequeueProcess(&readyQ);
+ 			nextElement->waitingTime++;
+ 			enqueueProcess(&readyQ, nextElement);
+ 		}
+
+		/* Updating the CPU */
+
+ 		for (i = 0; i < NUMBER_OF_PROCESSORS; i++){
+ 			if(cpu[i] != NULL){
+ 				cpu[i]->bursts[cpu[i]->currentBurst].step++;
+ 				cpu[i]->quantumRemaining--;
+ 			}
+ 		}
+
+		/* While loop will exit when total running process, total coming 
+		processes will be zero. And will also check if the size of waiting 
+		queue is zero or not. If yes then the loop will break otherwise not. */
+		
+		if (totalRunningProcesses() == 0 && totalIncomingProcesses() == 0 && waitQ.size == 0){
+			break;
+		}
+
+		/* CPU time will be incremented with running processes */
+
+		cpuTime += totalRunningProcesses();
+
+		/* Increments total time by 1 */
+
+		totalTime++;
+	}
+
+	/* Calculate turnaround time and total wait time of the processes 
+	and at last we are calculating the PID of the last process */
+
+	for(i = 0; i < numberOfProcesses; i++){
+		turnaround +=processes[i].endTime - processes[i].arrivalTime;
+		totalWait += processes[i].waitingTime;
+		if (processes[i].endTime == totalTime){
+			lastPID = processes[i].pid;
+		}
+	}
+
+	/* Printing the report for Round Robin Algorithm */
+
+    printf("\n<--RR-Algorithm Report-->\n"
+		"Time Quantum\t\t\t:%d\n"
+		"Average wait time\t\t:%.2f units\n"
+		"Average turnaround time\t\t:%.2f units\n"
+		"Total time to finish processes\t:%d\n"
+		"Average CPU utilization\t\t:%.1f%%\n"
+		"Number of context Switces\t:%d\n" 
+		"PID of last process to finish\t:%d\n\n", 
+		timeQ,
+		totalWait / (double) numberOfProcesses, 
+		turnaround / (double) numberOfProcesses, 
+		totalTime, 
+		(cpuTime * 100.0) / totalTime, 
+		contextSwitches, 
+		lastPID);	
+	
+	return 0;
+}
